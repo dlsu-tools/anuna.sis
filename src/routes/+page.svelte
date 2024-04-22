d432480b63ac8c65832d146340d016a476b4c459
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2024-04-22 17:47:11 +0800

    added old default screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import PhotoCopyright from "./PhotoCopyright.svelte";
+    import SignIn from "./SignIn.svelte";
+</script>
+
+<div class="relative h-screen" id="herobg">
+    <div class="flex h-full">
+        <h1 class="flex flex-grow flex-col items-center justify-center font-fugazOne">
+            <span class="text-4xl text-white">Welcome to an</span>
+            <span class="px-10 py-3 text-5xl text-black" id="tagline">
+                Easier Enlistment Experience
+            </span>
+        </h1>
+        <div class="flex basis-2/6 flex-col items-center justify-center">
+            <div class="flex w-[90%] flex-col place-items-center rounded-md p-10">
+                <SignIn></SignIn>
+            </div>
+        </div>
+    </div>
+    <PhotoCopyright></PhotoCopyright>
+</div>
+
+<style lang="postcss">
+    #herobg {
+        background-image: linear-gradient(
+                20deg,
+                hsl(152 100% 22%) 10%,
+                rgba(0, 0, 0, 0.25),
+                hsl(152 100% 22%) 90%
+            ),
+            url("https://upload.wikimedia.org/wikipedia/commons/5/5f/De_La_Salle_University_%28DLSU%29_Campus%3B_Rizal_Memorial_Sports_Complex%3B_Harrison_Plaza_-_Aerial_Shot_%28Taft_Avenue%2C_Malate%2C_Manila%3B_2015-06-25%29.jpg");
+        background-size: cover;
+        background-position: center 60%;
+        /* filter: brightness(75%); */
+    }
+
+    #tagline {
+        background: linear-gradient(
+            110deg,
+            transparent 5%,
+            hsl(152 100% 22%) 5%,
+            hsl(152 100% 22%) 95%,
+            transparent 95%
+        );
+    }
+</style>
---
+++ src/routes/PhotoCopyright.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<p class="absolute bottom-1 right-3 text-xs text-white">
+    &#169; Image Photo by Patrickroque01 on Wikipedia; CC BY-SA 4.0 DEED;
+    <a
+        href="https://en.m.wikipedia.org/wiki/File:De_La_Salle_University_(DLSU)_Campus;_Rizal_Memorial_Sports_Complex;_Harrison_Plaza_-_Aerial_Shot_(Taft_Avenue,_Malate,_Manila;_2015-06-25).jpg"
+    >
+        Original File Link
+    </a>
+</p>

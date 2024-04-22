09e3a2202fc21018fa8b4081223d8b5f973af5cf
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2024-04-22 15:46:13 +0800

    added tailwind and geist

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{html,js,svelte,ts}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                geist: ["Geist", "sans-serif"],
+                geistMono: ["'Geist Mono'", "monospace"],
+            },
+        },
+    },
+    plugins: [],
+};

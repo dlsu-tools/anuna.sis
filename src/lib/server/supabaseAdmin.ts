c7109b27b75387d446f478547d5fa3c2a5bab477
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2024-05-02 02:25:11 +0800

    made admin to recommended settings

--- src/lib/server/supabaseAdmin.ts
+++ src/lib/server/supabaseAdmin.ts
@@ -1,10 +1,16 @@
 import { createClient } from "@supabase/supabase-js";
 import { SUPABASE_SERVICE_ROLE_KEY } from "$env/static/private";
 import { PUBLIC_SUPABASE_URL } from "$env/static/public";
+import type { Database } from "$lib/database";
 
-export const supabaseAdmin = createClient(PUBLIC_SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY, {
-    auth: {
-        autoRefreshToken: false,
-        persistSession: false,
+export const supabaseAdmin = createClient<Database>(
+    PUBLIC_SUPABASE_URL,
+    SUPABASE_SERVICE_ROLE_KEY,
+    {
+        auth: {
+            persistSession: false,
+            autoRefreshToken: false,
+            detectSessionInUrl: false,
+        },
     },
-}).auth.admin;
+);

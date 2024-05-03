bf21b3d01c8ad236818572434cbb84255430aab7
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2024-05-03 22:29:01 +0800

    last contributions commit for now

---
+++ supabase/seed.sql
@@ -0,0 +1,10 @@
+INSERT INTO colleges(name, acronym)
+VALUES ('College of Computer Studies', 'CCS'),
+       ('Gokongwei College of Engineering', 'GCOE'),
+       ('College of Science', 'COS'),
+       ('Brother Andrew Gonzalez FSC College of Education', 'COED'),
+       ('Ramon V. del Rosario College of Business', 'RVRCOB'),
+       ('School of Economics', 'SOE'),
+       ('College of Liberal Arts', 'CLA');
+
+       

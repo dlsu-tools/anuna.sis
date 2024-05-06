5d70beca5c68cd3a3f32d27459e0c5cdb445ad65
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2024-05-06 21:44:46 +0800

    kinda gave up on local dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import mkcert from "vite-plugin-mkcert";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [sveltekit(), mkcert()],
+    server: {
+        https: true, // It's an says its error but that's what the docs say. /shrug
+    },
 });
